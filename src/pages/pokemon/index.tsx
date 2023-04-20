ad6e7d69d0f7325885cafc3dac26f23a87d43fb9
QuocVN <ngvanquoc.pro@gmail.com>
2023-04-20 22:33:02 +0700

    Update pokemon layout

--- src/pages/pokemon/index.tsx
+++ src/pages/pokemon/index.tsx
@@ -1,17 +1,9 @@
-import { Card, Layout, Select } from 'antd'
-
-import { pokemonOptions } from 'constants/pokemon.constant'
-import { useAppDispatch, useAppSelector } from 'redux/hooks'
-import { selectName, setPokemonName } from 'redux/slices/pokemon.slice'
-import { useGetPokemonByNameQuery } from 'services/pokemon.service'
-
-const { Meta } = Card
+import { useGetPokemonListQuery } from 'services/pokemon.service'
+import { Pokemon } from 'types/pokemon.type'
 
 const About = () => {
-  const pokemonName = useAppSelector(selectName)
-  const dispatch = useAppDispatch()
   // Using a query hook automatically fetches data and returns query values
-  const { isLoading, data, error } = useGetPokemonByNameQuery(pokemonName)
+  const { isLoading, data, error } = useGetPokemonListQuery()
   // Individual hooks are also accessible under the generated endpoints:
   // const { isLoading, data, error } = pokemonApi.endpoints.getPokemonByName.useQuery(pokemonName)
 
@@ -20,30 +12,38 @@ const About = () => {
   // eslint-disable-next-line no-console
   console.log({ isLoading, data, error })
 
-  const handleChange = (value: string) => {
-    dispatch(setPokemonName(value))
-  }
-
   return (
-    <Layout className='p-20'>
-      <Select
-        defaultValue='venusaur'
-        style={{ width: 120 }}
-        onChange={handleChange}
-        options={pokemonOptions}
-      />
-
-      <Card
-        hoverable
-        style={{ width: 240, marginTop: 10 }}
-        cover={<img src={data?.sprites.front_shiny} alt={data?.name} />}
-      >
-        <Meta
-          title={`Name: ${data?.name}`}
-          description={`Type: ${data?.types.map((item: any) => item.type.name).join(', ')}`}
-        />
-      </Card>
-    </Layout>
+    <div className='container mx-auto p-20'>
+      <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-4'>
+        {data?.map((item: Pokemon) => (
+          <div className='w-full max-w-sm rounded-lg border border-gray-200 bg-gray-50 shadow'>
+            <div className='flex flex-col items-center pb-10'>
+              <img
+                className='mb-3 h-24 w-24 rounded-full shadow-lg'
+                src={item.image}
+                alt={item.name}
+              />
+              <h5 className='mb-1 text-xl font-medium text-gray-900'>{`Name: ${item.name}`}</h5>
+              <span className='text-sm text-gray-500'>{`Type: ${item.types}`} </span>
+              <div className='mt-4 flex space-x-3 md:mt-6'>
+                <button
+                  type='button'
+                  className='inline-flex items-center rounded-lg bg-gradient-to-r from-cyan-500 to-blue-500 px-4 py-2 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-cyan-300'
+                >
+                  Edit
+                </button>
+                <button
+                  type='button'
+                  className='inline-flex items-center rounded-lg bg-gradient-to-br from-pink-500 to-orange-400 px-4 py-2 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-pink-200'
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
